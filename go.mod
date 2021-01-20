2f61cf3026a4426234022d4af9cdfe2507eab2ac
michael zhou <michaelzhou24@gmail.com>
2021-01-19 18:33:42 -0800

    finished most logic, need to fix concurrency, race conditions

--- go.mod
+++ go.mod
@@ -3,4 +3,3 @@ module example.org/cpsc416/a2
 go 1.14
 
 require github.com/DistributedClocks/tracing v0.0.0-20210116202148-0bd81866e504
-
--- hash_miner/hash_miner.go
+++ hash_miner/hash_miner.go
@@ -1,7 +1,13 @@
 package hash_miner
 
 import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
 	"github.com/DistributedClocks/tracing"
+	"math"
+	"strings"
+	"sync"
 )
 
 type WorkerStart struct {
@@ -23,13 +29,109 @@ type MiningComplete struct {
 	Secret []uint8
 }
 
+var succeeded = make(chan WorkerSuccess)
+
+func MineSuffix(wg *sync.WaitGroup, tracer *tracing.Tracer, startingPrefix uint8, nonce []uint8, numTrailingZeroes uint, threadBits uint) {
+	//fmt.Printf("Created worker with prefix: %#b\n", startingPrefix)
+	tracer.RecordAction(WorkerStart{startingPrefix})
+	defer wg.Done()
+	queue := make([][]uint8, 0)
+	startingArr := new(bytes.Buffer)
+	startingArr.WriteByte(startingPrefix << (8-threadBits))
+	if checkMinedValue(nonce, numTrailingZeroes, startingArr.Bytes()) {
+		success := WorkerSuccess{startingPrefix, startingArr.Bytes()}
+		tracer.RecordAction(success)
+		succeeded <-success
+		return
+	}
+	queue = append(queue, startingArr.Bytes())
+	//fmt.Printf("q: %#b\n", queue[0][0])
+	// check if starting suffix is enough
+	// Handle first byte
+	// 2^(8-threadbits) * 2^(8-threadbits) to do
+
+	for i := 0; i < int(math.Exp2(float64(8-threadBits))); i++ {
+		select {
+		case <-succeeded:
+			tracer.RecordAction(WorkerCancelled{startingPrefix})
+			return
+		default:
+			cpy := make([]uint8, len(queue[0]))
+			copy(cpy, queue[0])
+			cpy[0] |= uint8(i)
+			// check if correct hash
+			if checkMinedValue(nonce, numTrailingZeroes, cpy) {
+				success := WorkerSuccess{startingPrefix, cpy}
+				tracer.RecordAction(success)
+				succeeded <-success
+				return
+			}
+			queue = append(queue, cpy)
+		}
+	}
+	queue = queue[1:]
+
+	for {
+		select {
+		case <-succeeded:
+			tracer.RecordAction(WorkerCancelled{startingPrefix})
+			return
+		default:
+			for i := 0; i < 0x100; i++ {
+				curSecret := new(bytes.Buffer)
+				curSecret.Write(queue[0])
+				curSecret.WriteByte(reverseBits(byte(i)))
+				if checkMinedValue(nonce, numTrailingZeroes, curSecret.Bytes()) {
+					success := WorkerSuccess{startingPrefix, curSecret.Bytes()}
+					tracer.RecordAction(success)
+					succeeded <-success
+					return
+				}
+				queue = append(queue, curSecret.Bytes())
+			}
+			queue = queue[1:]
+		}
+	}
+}
+
+func reverseBits(b byte) byte{
+	b = (b & 0xF0) >> 4 | (b & 0x0F) << 4
+	b = (b & 0xCC) >> 2 | (b & 0x33) << 2
+	b = (b & 0xAA) >> 1 | (b & 0x55) << 1
+	return b
+}
+
+func checkMinedValue(nonce []uint8, numTrailingZeroes uint, secret []uint8) bool {
+	select {
+	case <-succeeded:
+		return false
+	default:
+		var compareString string
+		for i := uint(0); i < numTrailingZeroes; i++ {
+			compareString += "0"
+		}
+		concat := append(nonce, secret...)
+		checksum := md5.Sum(concat)
+		hashString := fmt.Sprintf("%x", checksum)
+		return strings.HasSuffix(hashString, compareString)
+	}
+}
+
 func Mine(tracer *tracing.Tracer, nonce []uint8, numTrailingZeroes, threadBits uint) (secret []uint8, err error) {
 	tracer.RecordAction(MiningBegin{})
 
 	// TODO
 
-	result := []uint8{}
+	// initialize threads
+	var wg sync.WaitGroup
 
+	for i := uint8(0); i < uint8(math.Exp2(float64(threadBits))); i++ {
+		wg.Add(1)
+		go MineSuffix(&wg, tracer, i, nonce, numTrailingZeroes, threadBits)
+	}
+	//wg.Wait()
+	success := <-succeeded
+	result := success.Secret
 	tracer.RecordAction(MiningComplete{result})
 
 	return result, nil
