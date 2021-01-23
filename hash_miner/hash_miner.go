98fd05707d65f0ba6eda0df5aecba5b8d5e3cc71
michael zhou <michaelzhou24@gmail.com>
2021-01-22 16:21:49 -0800

    working, need furthur optimization

--- hash_miner/hash_miner.go
+++ hash_miner/hash_miner.go
@@ -2,8 +2,8 @@ package hash_miner
 
 import (
 	"bytes"
-	"container/list"
 	"crypto/md5"
+	"encoding/binary"
 	"fmt"
 	"github.com/DistributedClocks/tracing"
 	"math"
@@ -35,7 +35,7 @@ var foundAns bool = false
 func mineWorker(waitgroup *sync.WaitGroup, tracer *tracing.Tracer, startingPrefix uint8, nonce []uint8, numTrailingZeroes uint, threadBits uint) {
 	defer waitgroup.Done()
 	tracer.RecordAction(WorkerStart{startingPrefix})
-	queue := list.New()
+	firstByteCombos := make([][]byte, 0)
 	startingArr := new(bytes.Buffer)
 	startingArr.WriteByte(startingPrefix << (8-threadBits))
 	if checkMinedValue(nonce, numTrailingZeroes, startingArr.Bytes()) {
@@ -44,9 +44,8 @@ func mineWorker(waitgroup *sync.WaitGroup, tracer *tracing.Tracer, startingPrefi
 		succeeded <-success
 		return
 	}
-	queue.PushBack(startingArr.Bytes())
-	startingArr.Reset()
-	//fmt.Printf("q: %#b\n", queue[0][0])
+	firstByteCombos = append(firstByteCombos, startingArr.Bytes())
+	//fmt.Printf("q: %#b\n", firstByteCombos[0][0])
 	// check if starting suffix is enough
 	// Handle first byte
 	// 2^(8-threadbits) * 2^(8-threadbits) to do
@@ -56,46 +55,45 @@ func mineWorker(waitgroup *sync.WaitGroup, tracer *tracing.Tracer, startingPrefi
 			tracer.RecordAction(WorkerCancelled{startingPrefix})
 			return
 		}
-		cpy := make([]uint8, len(queue.Front().Value.([]byte)))
-		copy(cpy, queue.Front().Value.([]byte))
+		cpy := make([]uint8, len(firstByteCombos[0]))
+		copy(cpy, firstByteCombos[0])
 		cpy[0] |= uint8(i)
 		// check if correct hash
 		if checkMinedValue(nonce, numTrailingZeroes, cpy) {
 			success := WorkerSuccess{startingPrefix, cpy}
+			foundAns = true
 			tracer.RecordAction(success)
 			succeeded <-success
 			return
 		}
-		queue.PushBack(cpy)
+		firstByteCombos = append(firstByteCombos, cpy)
 	}
-	queue.Remove(queue.Front())
 
-	for {
-		if foundAns {
-			tracer.RecordAction(WorkerCancelled{startingPrefix})
-			return
-		}
-
-		for i := 0; i < 0x100; i++ {
+	for i := uint64(0); i < math.MaxUint64; i++ {
+		for idx := 0; idx < len(firstByteCombos); idx++ {
 			if foundAns {
 				tracer.RecordAction(WorkerCancelled{startingPrefix})
 				return
 			}
-			curSecret := new(bytes.Buffer)
-			curSecret.Write(queue.Front().Value.([]byte))
-			curSecret.WriteByte(reverseBits(byte(i)))
-			if checkMinedValue(nonce, numTrailingZeroes, curSecret.Bytes()) {
-				success := WorkerSuccess{startingPrefix, curSecret.Bytes()}
+			buf := new(bytes.Buffer)
+			buf.WriteByte(firstByteCombos[idx][0])
+			if i <= math.MaxUint8 {
+				_ = binary.Write(buf, binary.LittleEndian, uint8(i))
+			} else if i <= math.MaxUint16 {
+				_ = binary.Write(buf, binary.LittleEndian, uint16(i))
+			} else if i <= math.MaxUint32 {
+				_ = binary.Write(buf, binary.LittleEndian, uint32(i))
+			} else {
+				_ = binary.Write(buf, binary.LittleEndian, i)
+			}
+			if checkMinedValue(nonce, numTrailingZeroes, buf.Bytes()) {
+				success := WorkerSuccess{startingPrefix, buf.Bytes()}
 				foundAns = true
 				tracer.RecordAction(success)
-				succeeded <- success
-				close(succeeded)
+				succeeded <-success
 				return
 			}
-			queue.PushBack(curSecret.Bytes())
-			curSecret.Reset()
 		}
-		queue.Remove(queue.Front())
 	}
 }
 
@@ -126,12 +124,25 @@ func Mine(tracer *tracing.Tracer, nonce []uint8, numTrailingZeroes, threadBits u
 	tracer.RecordAction(MiningBegin{})
 
 	// TODO
+	var success = WorkerSuccess{0, nil}
 	var waitgroup sync.WaitGroup
+	flag := 0
 	for i := 0; i < int(math.Exp2(float64(threadBits))); i++ {
-		waitgroup.Add(1)
-		go mineWorker(&waitgroup, tracer, uint8(i), nonce, numTrailingZeroes, threadBits)
+		if flag == 1 {
+			break
+		}
+		select {
+			case success = <-succeeded:
+				flag = 1
+				break
+			default:
+				waitgroup.Add(1)
+				go mineWorker(&waitgroup, tracer, uint8(i), nonce, numTrailingZeroes, threadBits)
+		}
 	}
-	success := <-succeeded
+	if success.Secret == nil {
+		success = <-succeeded
+	}
 	waitgroup.Wait()
 	result := success.Secret
 
